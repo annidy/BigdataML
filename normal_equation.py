2247171dd2b0eb618adf10f9a2f3fea2171b0c51
Feng Xing <andy@Fengs-iMac.local>
2018-08-18 17:23:48 +0800

    normal equation

---
+++ normal_equation.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt
+import sys
+
+'''
+Normal equation
+'''
+
+
+X = np.array([1,2,4,6])
+Y = np.array([0.8,1.3,1.9,5])
+
+X1 = np.vstack((np.ones(len(X)), X)).T
+
+t = np.linalg.inv(X1.T @ X1) @ X1.T @ Y
+
+
+plt.title('')
+
+# 画图
+# 1.真实的点
+plt.scatter(X, Y, color='blue')
+ 
+# 2.拟合的直线
+plt.plot(X,  X1 @ t, color='red')
+
+plt.show()
